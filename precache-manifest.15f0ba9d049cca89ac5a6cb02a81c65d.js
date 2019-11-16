da8e1df87ba338a66555130da858e83b0cf50660
salsyabila <salsyabila998@gmail.com>
2019-11-16 13:59:39 +0700

    init

---
+++ precache-manifest.15f0ba9d049cca89ac5a6cb02a81c65d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915e40f405ea9cc2b74c495587ec8b1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f7fbe5cab97db420de9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2fae7386e5df350ced0b",
+    "url": "/static/js/2.2519c88d.chunk.js"
+  },
+  {
+    "revision": "5f7fbe5cab97db420de9",
+    "url": "/static/js/main.1b8b889e.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "473244f60c9b79d55fb8034b71cf1071",
+    "url": "/static/media/bg.473244f6.png"
+  }
+]);
